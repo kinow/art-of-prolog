ac0310422004e3a04cec0c6b93d84c14dc62c15e
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-02-12 21:39:30 +0100

    Add plus_ (avoid redefining built in plus)

--- chapter03/arithmetic.pl
+++ chapter03/arithmetic.pl
@@ -1,8 +1,14 @@
 natural_number(X):-
 	between(0, inf, X).	
 
-
 less_than_or_equal(X, Y):-
 	natural_number(X),
 	natural_number(Y),
 	X =< Y.
+
+plus_(X, Y, Z):-
+	natural_number(X),
+	natural_number(Y),
+	natural_number(Z),
+	Z is X + Y.
+	
