e7eae6e7eb46d0affd340475c4474574439b3400
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-02-12 22:44:41 +0100

    Add multiplication

--- chapter03/arithmetic.pl
+++ chapter03/arithmetic.pl
@@ -9,6 +9,15 @@ less_than_or_equal(X, Y):-
 plus_(X, Y, Z):-
 	natural_number(X),
 	natural_number(Y),
-	natural_number(Z),
 	Z is X + Y.
+
+times(0, _, 0).
+times(X, Y, Z):-
+	natural_number(X),
+	natural_number(Y),
+	X > 0,
+	X1 is X - 1,
+	times(X1, Y, Z1),
+	plus_(Y, Z1, Z).
+
 	
